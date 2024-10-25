7267e81d36e77557fc44ac6122cdba57a3b66888
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-10-25 11:01:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 //your JS code here. If required.
 
-let container = document.querySelectorAll(".container");
-// console.log(container);
+// const container = document.querySelector(".container");
+// for(let i=0; i<800; i++){
+// 	const square = document.createElement("div")
+// 	square.classList.add("square")
+// 	square.addEventListener(⁠ mouseover ⁠, ()=>{
+// 		const color = ⁠ #${Math.floor(Math.random()*16777215).toString(16)} ⁠
+// 		        square.style.backgroundColor = color 
+// 	})
+// 							square.addEventListener("mouseout",()=>{
+// 		setTimeout(()=>{
+// 			square.style.backgroundColor = "rgb(29, 29, 29)"
+// 		}, 800)
+// 	})
+	
+// 	container.appendChild(square)
+// }
 
 
+let container = document.getElementsByClassName("container");
 
-for (let i = 0; i < 800; i++) {
+for (let i = 0; i<800; i++) {
     let box = document.createElement("div");
     container[0].append(box);
     // box.style.cssText = "height: 20px; width: 20px;";
@@ -13,21 +28,21 @@ for (let i = 0; i < 800; i++) {
 }
 let square = document.querySelectorAll(".square");
 
-square.forEach((sq) => {
-    sq.addEventListener("mouseover", () => {
-        sq.style.backgroundColor =  randomColor();
+square.forEach((e) => {
+    e.addEventListener("mouseover", () => {
+        e.style.backgroundColor =  randomColor();
         
         setTimeout(() => {
-            sq.style.backgroundColor = '';
-        }, 1000)
+            e.style.backgroundColor = "#";
+        }, 3000)
     })
 })
 
 function randomColor() {
     let letters = '0123456789ABCDEF'
     let color = "#"
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random()*16)]
+    for (let i = 0; i<6; i++) {
+        color += letters[parseInt(Math.random()*16)]
     }
     return color
 }
